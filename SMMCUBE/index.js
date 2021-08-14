182fe6eab1d5ea9489c9e283802fa4f149791106
CoderUmar <87554140+CoderUmar@users.noreply.github.com>
2021-08-14 15:59:40 +0500

    Add files via upload

---
+++ SMMCUBE/index.js
@@ -0,0 +1,8 @@
+toggle = document.getElementById('toggle');
+naw = document.getElementById('nawUl')
+    function bos(){
+      toggle.classList.toggle('active')   
+      naw.classList.toggle('navbarVis')
+}
+
+
